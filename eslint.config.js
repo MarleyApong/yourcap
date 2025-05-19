96fb77fc3fb1ba6af1e3b7127069afab2c01e17b
MarleyApong <108243674+MarleyApong@users.noreply.github.com>
2025-05-19 11:21:25 +0100

    chore: add Prettier and ESLint config, update TypeScript and package dependencies
    Added Prettier configuration and integrated it with ESLint for consistent code formatting. Updated TypeScript configuration to include custom type definitions. Added new dependencies for ESLint and Prettier integration, and updated existing dependencies to their latest versions. This ensures better code quality and maintainability.

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,35 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+// eslint.config.js
+import expoConfig from "eslint-config-expo/flat"
+import prettierPlugin from "eslint-plugin-prettier"
+import reactHooks from "eslint-plugin-react-hooks"
+import tseslint from "typescript-eslint"
 
-module.exports = defineConfig([
+export default tseslint.config([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ["dist/*"],
   },
-]);
+  {
+    plugins: {
+      prettier: prettierPlugin,
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "prettier/prettier": "error",
+
+      "no-unused-vars": "warn",
+      "no-console": "warn",
+      "no-debugger": "warn",
+      "prefer-const": "warn",
+      "no-shadow": "warn",
+      "no-multiple-empty-lines": ["warn", { max: 1 }],
+
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-non-null-assertion": "warn",
+    },
+  },
+])
